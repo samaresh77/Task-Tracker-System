d9612a8feaebe71b5163d02b6e58bf82565804c0
samaresh77 <samareshmondal53547@gmail.com>
2026-04-15 10:14:44 +0530

    models add

---
+++ server/src/models/user.model.ts
@@ -0,0 +1,17 @@
+import { pool } from "../config/db";
+
+export const createUser = async (name: string, email: string, password: string) => {
+  const result = await pool.query(
+    "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *",
+    [name, email, password]
+  );
+  return result.rows[0];
+};
+
+export const findUserByEmail = async (email: string) => {
+  const result = await pool.query(
+    "SELECT * FROM users WHERE email = $1",
+    [email]
+  );
+  return result.rows[0];
+};
